2c29de3029fdda495514433f2b6d714696932bc0
saramalka <sari.shlomovitz@gmail.com>
2025-01-08 20:04:43 +0200

    update func

--- branches.js
+++ branches.js
@@ -1,18 +1,5 @@
 
 const app1=()=>{
-console.log(`branch 1`)
+return "pull requset"
 }
 
-
-
-const app = () => {
-    return 17;
-  };
-
-const app3=()=>{
-return '1'
-}
-const app2=()=>{
-alert("hi branch-2")
-
-}
